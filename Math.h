5bcbd3e64233dd69af2c932f50c851f89283f994
京平 羽田 <210495@jc-21.jp>
2023-02-09 10:51:04 +0900

    逆ベクトル関数を作成

--- Math.h
+++ Math.h
@@ -1,9 +1,11 @@
 #pragma once
+#include "Engine/Transform.h"
 
 namespace Math {
 
 float EaseIn(float* speed, float acceleration, float max);
 float EaseOut(float* speed, float acceleration, float min);
+XMVECTOR Normal(XMVECTOR dir);
 
 }
 
